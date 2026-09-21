17d5515fa48d22a05bb18395f8eaf0050b925d4d
Prasit7 <159733083+Prasit7@users.noreply.github.com>
2026-09-20 20:39:47 -0500

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        //make freq map (key is number, value is frequency)
+        unordered_map<int, int> freq; 
+        for(int num : nums)
+        {
+            freq[num]++;
+        }
+        //make buckets (indexes are frequency)
+        vector<vector<int>> buckets(nums.size()+1);
+        for(auto& counter : freq)
+        {
+            buckets[counter.second].push_back(counter.first);
+        }
+        vector<int> result;
+        for(int i = buckets.size()-1; i >= 0; i--)
+        {
+            //dont need this inner loop skips over it already
+            if(buckets[i].size() > 0)
+            {
+                for(int num : buckets[i])
+                {
+                    result.push_back(num);
+                }
+            }
+            else
+            {
+                continue;
+            }
+            if(result.size() == k)
+            {
+                return result;
+            }
+        }
+        return result;
+    }
+};
